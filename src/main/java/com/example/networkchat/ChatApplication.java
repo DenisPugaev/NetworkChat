e404d38318de1d0fcfdfa0ec952663ae1e30bfb2
Denis_Pugaev <106180555+DenisPugaev@users.noreply.github.com>
2022-07-06 22:24:30 +0300

    ChatClient

--- src/main/java/com/example/networkchat/ChatApplication.java
+++ src/main/java/com/example/networkchat/ChatApplication.java
@@ -1,16 +1,20 @@
 package com.example.networkchat;
 //created by PugaevDenis
-
 import com.example.networkchat.controllers.ChatController;
 import com.example.networkchat.models.Network;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-
-
+/*
+Задача:
+Написать консольный вариант клиент\серверного приложения, в котором пользователь может писать сообщения, как на
+клиентской стороне, так и на серверной. Т.е. если на клиентской стороне написать "Привет", нажать Enter то сообщение
+должно передаться на сервер и там отпечататься в консоли. Если сделать то же самое на серверной стороне, сообщение
+соответственно передается клиенту и печатается у него в консоли. Есть одна особенность, которую нужно учитывать: клиент
+или сервер может написать несколько сообщений подряд, такую ситуацию необходимо корректно обработать
+*/
 public class ChatApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
